38063a8ce0d730558eaac759144ac524303ea4cf
sandi <sandi@LAPTOP-2FSFOPCR>
2024-10-27 11:56:08 +0530

    first commit

---
+++ git/main/git.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : git.c
+ Author      : Kajal Gangadhare
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a=5,b=10;
+	a =a+b;
+	printf("a=%d b=%d\n",a,b);
+
+	return EXIT_SUCCESS;
+}
